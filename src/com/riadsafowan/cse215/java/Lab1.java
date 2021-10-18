c85c066410a472d6e910b2c672466c2ed5954261
RiadSafowan1 <riadsafowan000@gmail.com>
2021-10-18 23:42:48 +0600

    fix

--- src/com/riadsafowan/cse215/java/Lab1.java
+++ src/com/riadsafowan/cse215/java/Lab1.java
@@ -7,20 +7,21 @@ import java.util.Scanner;
 public class Lab1 {
     public static void main(String[] args) {
 
-        //1. Write a program that prints your name, age and department in console.
+//      1. Write a program that prints your name, age and department in console.
         System.out.println("Name: Riad Safowan\n" +
                 "Age: 21\n" +
                 "Department: ECE");
+
         Utils.drawHorizontalLine();
 
-        //2. Write a program that prints from 1 to 10.
+//      2. Write a program that prints from 1 to 10.
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
         Utils.drawHorizontalLine();
 
-        //3. Write a program that determines if an integer is odd or even.
-        System.out.println("\nEnter a number: ");
+//      3. Write a program that determines if an integer is odd or even.
+        System.out.print("\nEnter a number: ");
         Scanner scanner = new Scanner(System.in);
         int inputNum = scanner.nextInt();
         if (inputNum % 2 == 0) {
