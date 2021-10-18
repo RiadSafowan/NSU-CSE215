3ea544454653bf2f8c13c5883de852b7b161290e
RiadSafowan1 <riadsafowan000@gmail.com>
2021-10-18 17:20:30 +0600

    Lab 1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static int a = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Test.kt
@@ -0,0 +1,19 @@
+class Test {
+    fun run() {
+        println("hello")
+    }
+}
+
+fun main() {
+    println("hello world")
+    val xx = Test()
+    xx.run()
+    val test = Test()
+    test.run()
+    haha()
+    Lab1.main(arrayOf("s"))
+}
+
+fun haha() {
+    println("haha")
+}
